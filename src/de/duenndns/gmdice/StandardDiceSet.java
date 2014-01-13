8f2b1d03a1e96bd8bb988dfc4e79b6a97920fbb7
Georg Lukas <georg@op-co.de>
2014-01-13 11:54:53 +0100

    fix copyright headers

--- src/de/duenndns/gmdice/StandardDiceSet.java
+++ src/de/duenndns/gmdice/StandardDiceSet.java
@@ -1,5 +1,6 @@
 // GameMaster Dice
-// Copyright (C) 2011 Georg Lukas
+// Copyright (C) 2014 David Pflug
+// Copyright (C) 2011-2014 Georg Lukas
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
